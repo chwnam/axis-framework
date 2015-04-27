c2c26102017e3cfb777d3351191dc839e38c3bb0
changwoo <cs.chwnam@gmail.com>
2015-04-28 00:51:27 +0900

    code update.

--- includes/models/class-base-model.php
+++ includes/models/class-base-model.php
@@ -2,22 +2,22 @@
 
 namespace axis_framework\includes\models;
 
+use axis_framework\includes\controls;
 use axis_framework\includes\core;
+
 
 abstract class Base_Model {
 
 	use core\Loader_Trait;
 
-	/** @noinspection PhpUndefinedClassInspection */
-	/** @noinspection PhpUndefinedNamespaceInspection */
-	/** @var axis_framework\includes\controls\Base_Control */
+	/** @var \axis_framework\includes\controls\Base_Control */
 	protected $control = NULL;
 
 	public function __construct( $args = array() ) {
 
 		if( isset( $args['control'] ) ) {
 
-			$this->control = &$args['control'];
+			$this->set_control( $args['control'] );
 		}
 
 		if( isset( $args['loader'] ) ) {
@@ -26,16 +26,8 @@ abstract class Base_Model {
 		}
 	}
 
-	public function set_control( &$control ) {
+	public function set_control( controls\Base_Control $control ) {
 
-		$this->control = &$control;
-	}
-
-	public function notify_to_control( $signal, $param = array() ) {
-
-		if( $this->control && is_string( $signal ) && is_array( $param ) ) {
-
-			call_user_func_array( array( &$this->control, &$signal ), $param );
-		}
+		$this->control = $control;
 	}
 }
