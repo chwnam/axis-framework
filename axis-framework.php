b75c96068160b359e26ffe21cc8a6c4640f65e8a
changwoo <ep6tri@hotmail.com>
2015-05-10 14:21:06 +0900

    license added.

--- axis-framework.php
+++ axis-framework.php
@@ -6,7 +6,26 @@
  * Author: Changwoo Nam
  * Author URI: mailto://cs.chwnam@gmail.com
  * Version: 0.20.0000Alpha1
+ * License: LGPLv2 or later
  */
+
+/*
+Copyright (C) 2015 Changwoo Nam
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 namespace axis_framework;
 
