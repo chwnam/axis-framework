1bef7f9cd0e622fd37416528234a61fce99909c9
changwoo <ep6tri@hotmail.com>
2015-06-24 11:59:26 +0900

    기본 클래스 이외의 것들은 spl_autoload_register() 의 콜백 함수가 include 담당.

--- axis-defines.php
+++ axis-defines.php
@@ -20,18 +20,42 @@ require_once( AXIS_FRAMEWORK_PATH . '/core/class-query.php' );
 require_once( AXIS_FRAMEWORK_PATH . '/core/class-singleton.php' );
 require_once( AXIS_FRAMEWORK_PATH . '/core/class-loader.php' );
 require_once( AXIS_FRAMEWORK_PATH . '/controls/class-base-control.php' );
-
 require_once( AXIS_FRAMEWORK_PATH . '/models/class-base-model.php' );
-require_once( AXIS_FRAMEWORK_PATH . '/models/interface-entity.php' );  // entity interface must be earlier than entity model
-require_once( AXIS_FRAMEWORK_PATH . '/models/class-base-entity-model.php' );
 require_once( AXIS_FRAMEWORK_PATH . '/views/class-base-view.php' );
-require_once( AXIS_FRAMEWORK_PATH . '/views/class-generic-view.php' );
-
 require_once( AXIS_FRAMEWORK_PATH . '/forms/class-base-form.php' );
-
 require_once( AXIS_FRAMEWORK_PATH . '/contexts/class-base-context.php' );
-require_once( AXIS_FRAMEWORK_PATH . '/contexts/class-base-routing-context.php' );
 require_once( AXIS_FRAMEWORK_PATH . '/contexts/class-dispatch.php' );
+
+/**
+ * any others will be auto-loaded by callback function
+ * @since 0.20.1000
+ */
+spl_autoload_register(
+	function( $class_name ) {
+		// only think about axis_framework namespace
+		if( 0 !== strpos( $class_name , 'axis_framework' ) ) {
+			return;
+		}
+		$cls     = substr( $class_name, strrpos( $class_name, '\\' ) + 1 );
+		$matches = NULL;
+		if( preg_match( '/^(.+)_(.+)$/', $cls, $matches ) ) {
+			$part      = str_replace( '_', '-', strtolower( $matches[1] ) );
+			$component = strtolower( $matches[2] );
+			switch( $component ) {
+				case 'context':
+				case 'control':
+				case 'form':
+				case 'model':
+				case 'view':
+					$plural_form = $component . 's';
+					$file_name   = "class-{$part}-{$component}.php";
+					/** @noinspection PhpIncludeInspection */
+					require_once( AXIS_FRAMEWORK_PATH . "/{$plural_form}/{$file_name}" );
+				break;
+			}
+		}
+	}
+);
 
 /**
  * Version of Axis Framework.
